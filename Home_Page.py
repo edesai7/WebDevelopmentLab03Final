4b5f86f0b475ef53e620a8141cd1bd50649356a3
edesai7 <edesai9@gatech.edu>
2025-04-03 21:47:37 -0400

    Add files via upload

--- Home_Page.py
+++ Home_Page.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 # Title of App
-st.title("Web Development Lab03")
+st.title("🎶Suggestify- song recommendation app")
 
 # Assignment Data 
 # TODO: Fill out your team number, section, and team members
 
 st.header("CS 1301")
-st.subheader("Team XX, Web Development - Section X")
-st.subheader("Team Member 1, Team Member 2")
+st.subheader("Team 33, Web Development - Section A")
+st.subheader("Elina Desai, Tamer Nawam")
 
 
 # Introduction
@@ -21,10 +21,11 @@ st.subheader("Team Member 1, Team Member 2")
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1.
-2.
-3.
-4.
+1. **Home Page**: You're here! 
+2. **Song Recommender**: Pick a mood or vibe, and we will recommend songs using a real music API.
+3. **AI DJ**: Our AI DJ writes fun intros, themed playlists, and music-based messages.
+4. **Lyric Analyzer Chatbot**: Paste in your favorite lyrics and chat with our AI to explore the meaning behind the words.
 
 """)
 
+st.image("Images/MusicWave.jpg", caption="Find your vibe 🎶", use_column_width=True)
