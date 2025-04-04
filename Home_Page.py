45ada762c92170d8fe7b455c95dbbd68fd0dc84c
edesai7 <edesai9@gatech.edu>
2025-04-03 21:54:59 -0400

    Add files via upload

--- Home_Page.py
+++ Home_Page.py
@@ -27,5 +27,4 @@ Welcome to our Streamlit Web Development Lab03 app! You can navigate between the
 4. **Lyric Analyzer Chatbot**: Paste in your favorite lyrics and chat with our AI to explore the meaning behind the words.
 
 """)
-
-st.image("Images/MusicWave.jpg", caption="Find your vibe 🎶", use_column_width=True)
+st.image("Images/MusicWave.jpg", caption="Find your vibe 🎵", use_container_width=True)
